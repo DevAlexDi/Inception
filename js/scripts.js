1fa31e085e33dec76bc0677ba06dad8fcd01006d
unknown <keitmark@mail.ru>
2018-02-25 10:40:27 +0300

    mobile done

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,23 @@ $(document).ready(function () {
 });
 $(window).load(function() {
      var win = $(window);
+    
+
     var scrFunc = function () {
+        var padding;
+        var widthWin = $(window).width();
+        if(widthWin >991){
+            padding = 100;
+        }
+        else{
+            padding = 50;
+        }
         var t = win.scrollTop(),
             e = win.height();
         $("[data-anim], .main-baner .body-baner,.main-baner,.sec-developing,.for-flex-columns .side,.sec-lines .head,.sec-lines .flex-contant .left-side,.sec-lines .abs-lines,.sec-lines").each(function (n, i) {
             var r = $(i).offset().top,
                 s = t + .9 * e;
-            s - 100 > r ? $(i).attr("data-anim", "true") : $(i).attr("data-anim", "false");
+            s - padding > r ? $(i).attr("data-anim", "true") : $(i).attr("data-anim", "false");
             
             if($('.sec-lines').attr('data-anim') == 'true'){
                 $('.footer').addClass("show");
